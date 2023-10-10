0bc507a4f884553de37170c453f8979d952ab066
NguyenCaoThi <caothiu2003@gmail.com>
2023-10-10 23:28:08 +0700

    api signin edit

--- Server/Source/controllers/API_Signin_Signup_ctrl.py
+++ Server/Source/controllers/API_Signin_Signup_ctrl.py
@@ -1,10 +1,37 @@
 from Source import app
 from Source.models_mvc.accoout_model import Account
+import random
+from Source.controllers.sendMail_class import Maill
 
-@app.post('/login')
-async def login(item: Account):
-    result=item.insertAccout()
+
+
+@app.post('/signin')
+async def signin(body: Account):
+    result=body.checkAccount()
     if result==True:
-        return "Create account successful"
+        return "Login successfull"
     else: 
-        return "Username already exists!"
+        return "Incorrect username or password!"
+    
+@app.post('/signup')
+def signup(body:Account):
+    user=body.existenceUsername()
+    if user==True:
+        return "Username already exists"
+    email=body.existenceEmail()
+    if email==True:
+        return "Username already exists"
+    OTP=random.randint(100000,999999)
+    sendMail=Maill()
+    sendMail.sendOTP(body.email,OTP)
+    body.addOTP(OTP)
+    return "Enter OTP"
+
+    
+@app.post('/signup/verify')
+def verify(body:Account):
+    result=body.checkOTP()
+    if result==True:
+        body.insertAccout()
+        return "Signup successfull"
+    return "Please re-enter OTP"
--- Server/Source/models_mvc/accoout_model.py
+++ Server/Source/models_mvc/accoout_model.py
@@ -1,10 +1,22 @@
 from pydantic import BaseModel
 from firebase_admin import db
+import threading
 
 class Account(BaseModel):
     username: str
     password: str
     email: str=None
+    OTP:str=None
+
+    def checkAccount(sefl):
+        ref=db.reference(f"Accounts/{sefl.username}")
+        userData=ref.get()
+        if userData==None:
+            return False
+        if userData['password']!=sefl.password:
+            return False
+        return True
+
     def insertAccout(sefl):
         ref=db.reference('Accounts')
         checkUsername=sefl.existenceUsername()
@@ -12,13 +24,40 @@ class Account(BaseModel):
             new_user_ref = ref.child(sefl.username)
             new_user_ref.set({
                 'username': sefl.username,
-                'password': sefl.password
+                'password': sefl.password,
+                'email':sefl.email
             })
             return True
         else: 
             return False
+        
+    def addOTP(sefl,OTP):
+        ref=db.reference("OTP_email").child(sefl.username)
+        ref.set(f"{OTP}")
+        def deleteOTP():
+            ref=db.reference(f'OTP_email/{sefl.username}')
+            ref.delete()
+        # Hẹn giờ tác vụ sau 2 phút để xóa OTP
+        timer = threading.Timer(2*60,deleteOTP)
+        timer.start()
 
+    def checkOTP(sefl):
+        ref=db.reference(f"OTP_email/{sefl.username}")
+        OTP=ref.get()
+        if OTP==None or OTP!=f"{sefl.OTP}":
+            return False
+        return True
+    
     def existenceUsername(sefl):
         ref=ref=db.reference('Accounts')
-        result=ref.child(sefl.username).get()
-        return result
+        user=ref.child(sefl.username).get()
+        if user==None:
+            return False
+        return True
+    
+    def existenceEmail(sefl):
+        ref=ref=db.reference('Accounts')
+        user=ref.order_by_child('email').equal_to(sefl.email)
+        if user==None:
+            return False
+        return True
