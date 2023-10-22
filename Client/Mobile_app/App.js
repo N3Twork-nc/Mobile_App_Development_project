b2aba745b4b76e4d8c7bdc82e31459a818a2b89c
chiecbungbaton <21522371@gm.uit.edu.vn>
2023-10-22 23:45:47 +0700

    Đăng nhập thành công, sửa xíu hàm gọi API

--- Client/Mobile_app/App.js
+++ Client/Mobile_app/App.js
@@ -3,9 +3,17 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Home from './src/components/Home/home';
 import Signin from './src/components/Sign in/signin'
+import AppNavigator from './src/navigators/AppNavigator';
 
+const App = () =>
+{
+  return <AppNavigator/>
+}
+export default App;
+{/*--
 export default function App() {
   return <NavigationContainer>
-          <Home/>
+          <Signin/>
          </NavigationContainer>
 };
+*/}
--- Client/Mobile_app/src/navigators/AppNavigator.js
+++ Client/Mobile_app/src/navigators/AppNavigator.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Signin from '../components/Sign in/signin';
 import Signup from '../components/Sign up/signup';
+import Home from '../components/Home/home';
 
 const Stack = createStackNavigator();
 
@@ -14,6 +15,7 @@ const AppNavigator = () => {
         }}>
         <Stack.Screen name="SignIn" component={Signin} />
         <Stack.Screen name="SignUp" component={Signup} />
+        <Stack.Screen name="Home" component={Home}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
