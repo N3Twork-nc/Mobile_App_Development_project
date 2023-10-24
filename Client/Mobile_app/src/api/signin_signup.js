79b955ab4005b3e3a0844cc469026d0dd37a423a
Anh Thu <anhthu@github.com>
2023-10-24 10:59:26 +0700

    sửa apiUrl thành biến toàn cục

--- Client/Mobile_app/src/api/signin_signup.js
+++ Client/Mobile_app/src/api/signin_signup.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 
+const apiUrl = 'http://10.0.133.77:8080';
+
 export const signin = (username, password) => {
   const data = {
-    username: username,
-    password: password
+    username: `${username}`,
+    password: `${password}`
   };
 
   return new Promise((resolve, reject) => {
-    axios
-      .post('http://10.0.128.98:8080/APIsignin', data)
+    axios.post(`${apiUrl}/APIsignin`, data)
       .then(response => {
         console.log(response.data); 
         resolve(response);
@@ -29,7 +30,7 @@ export const signup = (fullname, username, password, email) => {
   };
 
   return new Promise((resolve, reject) => {
-  axios.put('http://172.20.10.6:8080/APIsignup', data)
+  axios.put(`${apiUrl}/APIsignup`, data)
     .then(response => {
       console.log(response.data);
       resolve(response); // Xử lý phản hồi từ API
@@ -50,32 +51,14 @@ export const verify = (fullname, username, password, email, otp) => {
     "OTP": `${otp}`
   };
   return new Promise((resolve, reject) => {
-  axios.post('http://172.20.10.6:8080/APIsignup', data)
+  axios.post(`${apiUrl}/APIsignup`, data)
     .then(response => {
       console.log(response.data); // Xử lý phản hồi từ API
+      resolve(response);
     })
     .catch(error => {
       console.error(error);
+      reject(error);
     });
   });
 };
-
-// Gọi API xác minh
-// const verify = async () => {
-//   try {
-//     const response = await axios.post('https://26.242.101.156/APIsignup', {
-//       // Gửi dữ liệu tài khoản
-//       email,
-//     });
-
-//     // Xử lý kết quả xác minh
-//     console.log(response.data);
-//   } catch (error) {
-//     // Xử lý lỗi xác minh
-//     console.error(error);
-//   }
-// };
-
-// Gọi các hàm tương ứng với các API
-// signin('caothi','1234');
-
