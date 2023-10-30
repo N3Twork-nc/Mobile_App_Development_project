f2caf58702c1b1c7b5356b324573476fabfdb086
Anh Thu <anhthu@github.com>
2023-10-30 21:24:50 +0700

    thêm plant vào các phòng

--- Client/Mobile_app/App.js
+++ Client/Mobile_app/App.js
@@ -2,8 +2,12 @@ import React from 'react';
 import AppNavigator from './src/navigators/AppNavigator';
 import { Provider } from 'react-redux';
 import store from './src/store';
+
+import { plant } from './src/api/uploadPlant';
+
 const App = () =>
 {
+  plant("caothi", "Phòng ngủ", "Cây lưỡi hổ")
   return <Provider store={store}>
     <AppNavigator/>
   </Provider> 
