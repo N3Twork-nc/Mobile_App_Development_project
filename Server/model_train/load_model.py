cb24baee159960e779b4d13f4a68f5607eea89c4
chiecbungbaton <21522371@gm.uit.edu.vn>
2023-11-09 14:19:56 +0700

    animatin loading nhưng chưa hoàn thành

--- Server/model_train/load_model.py
+++ Server/model_train/load_model.py
@@ -2,10 +2,10 @@
 import numpy as np
 from keras.models import load_model
 
-model=load_model('./model_train/plants_EfficientNetB4_model256.h5')
+#model=load_model('./model_train/plants_EfficientNetB4_model256.h5')
 class Model:
     @staticmethod
     def PredictPlants(img):
-        resuft=model.predict(img)
-        y_pred=np.argmax(resuft, axis=1)
+       # resuft=model.predict(img)
+       # y_pred=np.argmax(resuft, axis=1)
         return y_pred
