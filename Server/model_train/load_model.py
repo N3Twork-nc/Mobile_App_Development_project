5db706e1e037160316ba8c27e15262290c8ebc18
Anh Thu <anhthu@github.com>
2023-11-09 20:56:52 +0700

    Merge branch 'AnhThu' of https://github.com/N3Twork-nc/Mobile_App_Development_project into AnhThu

--- Server/model_train/load_model.py
+++ Server/model_train/load_model.py
@@ -2,11 +2,7 @@
 import numpy as np
 from keras.models import load_model
 
-<<<<<<< HEAD
 # model=load_model('./model_train/plants_EfficientNetB4_model256.h5')
-=======
-#model=load_model('./model_train/plants_EfficientNetB4_model256.h5')
->>>>>>> 0f16c5f58a9fe4ab67e03f7bfa2552e19933c3fa
 class Model:
     @staticmethod
     def PredictPlants(img):
