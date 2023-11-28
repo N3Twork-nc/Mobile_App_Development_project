09ba3b37b8df6518f13cc4ac7cf7369ca6675368
ThaiNhatThu <21522648@gm.uit.edu.vn>
2023-11-28 21:11:20 +0700

    nothing

--- Server/Source/__init__.py
+++ Server/Source/__init__.py
@@ -3,13 +3,13 @@ from fastapi_mqtt import FastMQTT, MQTTConfig
 
 app = FastAPI()
 
-mqtt_config = MQTTConfig()
+# mqtt_config = MQTTConfig()
 
-mqtt = FastMQTT(
-    config=mqtt_config
-)
+# mqtt = FastMQTT(
+#     config=mqtt_config
+# )
 
-mqtt.init_app(app)
+# mqtt.init_app(app)
 
 
 from Source.config.config import *
