c7226bc02e5db9c77f51c670769cfa8fb91700e8
chiecbungbaton <21522371@gm.uit.edu.vn>
2023-12-06 18:31:05 +0700

    fix bug dashboard

--- Client/Mobile_app/src/components/Dashboard/dashboard.js
+++ Client/Mobile_app/src/components/Dashboard/dashboard.js
@@ -71,52 +71,6 @@ const DashBoard = () => {
   } else {
     imgSource = placeholder;
   }
-
-  //edit thông tin vườn
-  const [isEditVisible, setEditVisible] = useState(false);
-  const EditGarden = ({ isVisible, message, onSave, onCancel }) => {
-    return (
-      <Modal isVisible={isVisible}>
-        <View style={{ backgroundColor: 'white', padding: 12, borderRadius: 15}}>
-          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' ,marginBottom: 10, height: 50, }}>
-            <Image source={logoApp} style={{ width: 32, height: 32, marginBottom: 5, marginRight: 5 }} />
-            <Text style={{ fontSize: 17, fontWeight: '600', }}>{message}</Text>
-            <Image source={logoApp} style={{ width: 32, height: 32, marginBottom: 5,  marginLeft:5  }} />
-          </View>
-          <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 15,  }}>
-            <Text style={{ fontSize: 16, flex: 1, fontWeight: '500', marginLeft: 10 }}>Tên vườn:</Text>
-            <TextInput style={{ marginRight: 12, flex: 2, borderWidth: 1, borderColor: 'gray', borderRadius: 5, paddingHorizontal: 10, height: 30 }} />
-          </View>
-          <Image source={logoApp} style={{ width: 100, height: 50, marginBottom: 5,  marginLeft:5  }}/>
-          <View style={{ padding: 10, zIndex: 1}}>              
-            <View style={{ flexDirection: 'row', justifyContent: 'flex-end', }}>
-            <TouchableOpacity onPress={onCancel} style={{ marginRight: 25 }}>
-              <Text style={{ color: 'green', fontSize: 17 }}>Hủy</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={onSave}>
-              <Text style={{ color: 'green', fontSize: 17 }}>Lưu</Text>
-            </TouchableOpacity>
-          </View>
-        </View>        
-      </View>
-      </Modal>
-    )
-  };
-  const handleEdit = () =>
-  {
-    setEditVisible(true);
-  };
-  const handleSaveEdit = () =>
-  {
-    // xử lý việc lưu thông tin mới của vườn
-
-    setEditVisible(false);
-  };
-  const handleCancelEdit = () =>
-  {
-    setEditVisible(false);
-  }
- 
   
 
   return (
@@ -147,18 +101,11 @@ const DashBoard = () => {
               source={imgSource}/>
           </ImageContainer>
           <ButtonsContainer>
-            <EditContainer>              
-              <EditButton source={require("../../assets/edit.png")} tintColor='#164303'/>
-              <EditGarden
-                isVisible={isEditVisible}
-                message="Chỉnh sửa"
-                onCancel={handleCancelEdit}
-                onSave={handleSaveEdit}
-              />
+            <EditContainer>
+              <EditButton  resizeMode="cover" source={require("../../assets/edit.png")}/>
             </EditContainer>
-           
             <SwitchContainer>
-              <SwitchButton source={require("../../assets/lighton.png")} tintColor='#164303'/>
+              <SwitchButton  resizeMode="cover" source={require("../../assets/lighton.png")}/>
             </SwitchContainer>
           </ButtonsContainer>
         </GardenInfo>
