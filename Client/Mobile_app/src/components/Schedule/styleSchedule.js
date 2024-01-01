e563f090cf56d26e3c50a43625694c6ee78063bf
Anh Thu <anhthu@github.com>
2024-01-01 17:45:47 +0700

    done plant detail

--- Client/Mobile_app/src/components/Schedule/styleSchedule.js
+++ Client/Mobile_app/src/components/Schedule/styleSchedule.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Dimensions, Platform } from 'react-native';
+import { Dimensions, Platform, StatusBar } from 'react-native';
 
 //Lấy kích thước thah status
 const statusBarHeight = Platform.OS === 'android' ? StatusBar.currentHeight : 0;
