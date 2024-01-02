765822182b0809340b912ace29bfaaff5e1099e8
chiecbungbaton <21522371@gm.uit.edu.vn>
2024-01-02 11:48:28 +0700

    change Move to Delete plant

--- Client/Mobile_app/src/components/Home/Rooms/room.js
+++ Client/Mobile_app/src/components/Home/Rooms/room.js
@@ -4,10 +4,13 @@ import { StyledContainer, HeaderContainer, MainTitle, ButtonBack, BackContainer,
          ButtonContainer, IconButton, Icon, ButtonText, ButtonContainerWrapper,
         } from './styleRoom'
 import { useNavigation, useRoute } from '@react-navigation/native';
-import { ScrollView, SafeAreaView, StyleSheet, View } from 'react-native';
+import { Text, ScrollView, SafeAreaView, StyleSheet, View, Alert, Image, TouchableOpacity } from 'react-native';
 import { useSelector } from 'react-redux';
 import { getPlant } from '../../../api/Plant.js';
 import LottieView from 'lottie-react-native';
+import Modal from 'react-native-modal';
+import logo from '../../../assets/logo.png';
+const logoApp = logo;
 
 const Room = () => {
     const [isLoading, setIsLoading] = useState(false);
@@ -15,6 +18,30 @@ const Room = () => {
     const navigation = useNavigation();
     const route = useRoute();
     const { plantsInRoom, roomName } = route.params;
+
+    // Xử lý xóa cây
+    const [isAlertVisible, setAlertVisible] = useState(false);
+    const [deleteGardenId,setDeleteGardenId]=useState(null)
+    
+    const handleDelete = (garden) => {
+        setAlertVisible(true);
+        setDeleteGardenId(garden.gardenId)
+    };
+
+    const handleCancel = () => {
+        setAlertVisible(false);
+    };
+
+    const handleConfirmDelete = async () => {
+        setAlertVisible(false);
+        const result = await deleteGarden(deleteGardenId,token);
+        const gardenDetails = await getDetailGardens(token);
+        const action=updateMyGarden(gardenDetails)
+        dispatch(action)
+        if (result==true) Alert.alert ("Xóa vườn thành công");
+        else Alert.alert ("Xóa vườn thất bại");
+    };
+    
 
     const handleInfo = async (plantname) => {
         setIsLoading(true);
@@ -30,11 +57,11 @@ const Room = () => {
 
     const handleBack = () => {
     navigation.navigate('Home');
-  };
-  const handleSchedule = (idPlant) => {
-    navigation.navigate('Schedule',{idPlant,roomName});
-    
-  };
+    };
+    const handleSchedule = (idPlant) => {
+        navigation.navigate('Schedule',{idPlant,roomName});
+        
+    };
 return (
     <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
         <ScrollView style={{ flex: 1 }}>
@@ -70,9 +97,15 @@ return (
                                         <Icon source={require('../../../assets/water.png')} />
                                         <ButtonText>Đặt lịch</ButtonText>
                                     </IconButton>
-                                    <IconButton>
-                                        <Icon source={require('../../../assets/move.png')} />
-                                        <ButtonText>Di chuyển</ButtonText>
+                                    <IconButton onPress={handleDelete}>
+                                        <Icon source={require('../../../assets/bin.png')} />
+                                        <ButtonText>Xóa</ButtonText>
+                                        <CustomAlert
+                                            isVisible={isAlertVisible}
+                                            message="Bạn có chắc chắn muốn xóa cây này không?"
+                                            onCancel={handleCancel}
+                                            onDelete={handleConfirmDelete}
+                                            />
                                     </IconButton>
                                     </ButtonContainer>
                                 </ButtonContainerWrapper>                        
@@ -93,8 +126,8 @@ return (
                                         <ButtonText>Đặt lịch</ButtonText>
                                     </IconButton>
                                     <IconButton>
-                                        <Icon source={require('../../../assets/move.png')} />
-                                        <ButtonText>Di chuyển</ButtonText>
+                                        <Icon source={require('../../../assets/bin.png')} />
+                                        <ButtonText>Xóa</ButtonText>
                                     </IconButton>
                                     </ButtonContainer>
                                 </ButtonContainerWrapper>                        
@@ -130,3 +163,28 @@ const styles = StyleSheet.create({
         zIndex: 1,
     },
   });
+// Alert cảnh báo khi nhấn XÓA
+const CustomAlert = ({ isVisible, message, onCancel, onDelete }) => {
+
+  
+    return (
+      <Modal isVisible={isVisible}>
+        <View style={{ backgroundColor: 'white', padding: 15, borderRadius: 15}}>
+          <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10, height: 50, }}>
+            <Image source={logoApp} style={{ width: 32, height: 32, marginRight: 5, marginBottom: 5 }} />
+            <Text style={{ fontSize: 17, flex: 1, fontWeight: '500' }}>{message}</Text>
+          </View>
+          <View style={{ padding: 10, zIndex: 1,}}>              
+            <View style={{ flexDirection: 'row', justifyContent: 'flex-end', marginTop: 10 }}>
+              <TouchableOpacity onPress={onCancel} style={{ marginRight: 25 }}>
+                <Text style={{ color: 'green', fontSize: 17 }}>Hủy</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={onDelete}>
+                <Text style={{ color: 'red', fontSize: 17 }}>Xóa</Text>
+              </TouchableOpacity>
+            </View>
+          </View>        
+      </View>
+      </Modal>
+    );
+  };
