62b7440b258ff28bfcf688b9217d0c7ac932b965
ThaiNhatThu <21522648@gm.uit.edu.vn>
2024-01-04 12:49:31 +0700

    nothing

--- Client/Mobile_app/App.js
+++ Client/Mobile_app/App.js
@@ -7,33 +7,33 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import Schedule from './src/components/Schedule/schedule';
 
-// class App extends Component
-// {
-//   componentDidMount() {
-//     if (Platform.OS === 'android') {
-//       StatusBar.setBackgroundColor('white');
-//     }
-//   };
-//   render()
-//   {
-//     return <Provider store={store}>
-//     <AppNavigator/>
-//   </Provider> 
-//   }
+class App extends Component
+{
+  componentDidMount() {
+    if (Platform.OS === 'android') {
+      StatusBar.setBackgroundColor('white');
+    }
+  };
+  render()
+  {
+    return <Provider store={store}>
+    <AppNavigator/>
+  </Provider> 
+  }
   
-// }
-// export default App;
+}
+export default App;
 
 
-export default function App() {
-return (
-<Provider store={store}>
- <NavigationContainer>
-    <Schedule />
-   </NavigationContainer>
-  </Provider>
-);
-};  
+// export default function App() {
+// return (
+// <Provider store={store}>
+//  <NavigationContainer>
+//     <Schedule />
+//    </NavigationContainer>
+//   </Provider>
+// );
+// };  
 
 
 
--- Client/Mobile_app/src/api/index.js
+++ Client/Mobile_app/src/api/index.js
@@ -1,1 +1,1 @@
-export const IPServer="http://192.168.137.1:8080/"
+export const IPServer="http://192.168.100.154:8080/"
