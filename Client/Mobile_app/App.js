8c16b6828e9f4c752fe8aa9bafc4817f8d937ed2
chiecbungbaton <21522371@gm.uit.edu.vn>
2024-01-05 13:30:17 +0700

    fix giao diện

--- Client/Mobile_app/App.js
+++ Client/Mobile_app/App.js
@@ -1,13 +1,9 @@
-import React, {Component,useEffect} from 'react';
+import React, {Component} from 'react';
 import AppNavigator from './src/navigators/AppNavigator';
 import { Provider } from 'react-redux';
 import store from './src/store';
 import { StatusBar, Platform} from 'react-native';
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import Schedule from './src/components/Schedule/schedule';
-import ResetPass from './src/components/Profile/Reset Pass/resetpass';
-import CameraScreen from './src/components/Camera/camera';
 
 class App extends Component
 {
@@ -21,21 +17,9 @@ class App extends Component
     return <Provider store={store}>
     <AppNavigator/>
   </Provider> 
-  }
-  
+  }  
 }
-export default App;
-
-
-// export default function App() {
-// return (
-// <Provider store={store}>
-//  <NavigationContainer>
-//     <CameraScreen />
-//    </NavigationContainer>
-//   </Provider>
-// );
-// };  
+export default App;  
 
 
 
